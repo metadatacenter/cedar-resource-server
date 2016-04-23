cc3fe157b507530c7281f6e9be71ddf6e585e0a7
Marcos Martinez <marcos.martinez.romero@gmail.com>
2016-04-22 17:10:07 -0700

    Add util class with index-related operations

---
+++ cedar-resource-server-core/src/main/java/org/metadatacenter/cedar/resource/util/IndexUtil.java
@@ -0,0 +1,14 @@
+package org.metadatacenter.cedar.resource.util;
+
+public class IndexUtil {
+
+
+  public IndexUtil() {
+    
+  }
+
+  public static void indexResource(/* TODO: appropriate resource class here */) {
+
+  }
+
+}
