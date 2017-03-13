d9254d6f4108d180618fc60cdd28fb6fa3d14de0
johardi <josef.hardi@stanford.edu>
2017-03-13 10:52:11 -0700

    Implement the 'format' query option to find template instance

--- cedar-resource-server-application/src/main/java/org/metadatacenter/cedar/resource/resources/TemplateInstancesResource.java
+++ cedar-resource-server-application/src/main/java/org/metadatacenter/cedar/resource/resources/TemplateInstancesResource.java
@@ -19,6 +19,7 @@ import java.util.Optional;
 
 import static org.metadatacenter.constant.CedarPathParameters.PP_ID;
 import static org.metadatacenter.constant.CedarQueryParameters.QP_FOLDER_ID;
+import static org.metadatacenter.constant.CedarQueryParameters.QP_FORMAT;
 import static org.metadatacenter.constant.CedarQueryParameters.QP_IMPORT_MODE;
 import static org.metadatacenter.rest.assertion.GenericAssertions.LoggedIn;
 import static org.metadatacenter.rest.assertion.GenericAssertions.NonEmpty;
@@ -55,13 +56,14 @@ public class TemplateInstancesResource extends AbstractResourceServerResource {
   @GET
   @Timed
   @Path("/{id}")
-  public Response findTemplateInstance(@PathParam(PP_ID) String id) throws CedarException {
+  public Response findTemplateInstance(@PathParam(PP_ID) String id, @QueryParam(QP_FORMAT) Optional<String> format)
+      throws CedarException {
     CedarRequestContext c = CedarRequestContextFactory.fromRequest(request);
     c.must(c.user()).be(LoggedIn);
     c.must(c.user()).have(CedarPermission.TEMPLATE_INSTANCE_READ);
 
     userMustHaveReadAccessToResource(c, id);
-    return executeResourceGetByProxy(CedarNodeType.INSTANCE, id, c);
+    return executeResourceGetByProxy(CedarNodeType.INSTANCE, id, format, c);
   }
 
   @GET
