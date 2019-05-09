8b552c1a647767d85d9c6f6e0d5724b42257bc1a
Marcos Martinez <marcos.martinez.romero@gmail.com>
2019-05-09 13:19:16 -0700

    Add CEDAR_SUBMISSION_TEMPLATE_ID_2

--- cedar-resource-server-application/src/test/java/org/metadatacenter/cedar/resource/config/CedarConfigResourceTest.java
+++ cedar-resource-server-application/src/test/java/org/metadatacenter/cedar/resource/config/CedarConfigResourceTest.java
@@ -42,6 +42,7 @@ public class CedarConfigResourceTest {
     env.put(CedarEnvironmentVariable.CEDAR_VALIDATION_ENABLED.getName(), "false");
 
     env.put(CedarEnvironmentVariable.CEDAR_SUBMISSION_TEMPLATE_ID_1.getName(), "http://template-id-1");
+    env.put(CedarEnvironmentVariable.CEDAR_SUBMISSION_TEMPLATE_ID_2.getName(), "http://template-id-2");
 
     env.put(CedarEnvironmentVariable.CEDAR_ELASTICSEARCH_HOST.getName(), "127.0.0.1");
     env.put(CedarEnvironmentVariable.CEDAR_ELASTICSEARCH_TRANSPORT_PORT.getName(), "9300");
