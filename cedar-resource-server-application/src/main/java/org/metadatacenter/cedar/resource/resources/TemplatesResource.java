5ee52abccfe0c71d790ddb5796cf473a53c01e20
Attila Levente EGYEDI <attila.egyedi@gmail.com>
2024-09-18 16:04:52 -0700

    Download resource backend started

--- cedar-resource-server-application/src/main/java/org/metadatacenter/cedar/resource/resources/TemplatesResource.java
+++ cedar-resource-server-application/src/main/java/org/metadatacenter/cedar/resource/resources/TemplatesResource.java
@@ -1,16 +1,24 @@
 package org.metadatacenter.cedar.resource.resources;
 
 import com.codahale.metrics.annotation.Timed;
+import org.metadatacenter.bridge.CedarDataServices;
 import org.metadatacenter.config.CedarConfig;
+import org.metadatacenter.error.CedarErrorKey;
 import org.metadatacenter.exception.CedarException;
+import org.metadatacenter.id.CedarArtifactId;
 import org.metadatacenter.id.CedarTemplateId;
+import org.metadatacenter.id.CedarUntypedArtifactId;
 import org.metadatacenter.model.CedarResourceType;
+import org.metadatacenter.model.folderserver.basic.FolderServerArtifact;
 import org.metadatacenter.rest.context.CedarRequestContext;
+import org.metadatacenter.server.FolderServiceSession;
 import org.metadatacenter.server.security.model.auth.CedarPermission;
+import org.metadatacenter.util.http.CedarResponse;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.Arrays;
 import java.util.Optional;
 
 import static org.metadatacenter.constant.CedarPathParameters.PP_ID;
@@ -37,14 +45,31 @@ public class TemplatesResource extends AbstractResourceServerResource {
   @GET
   @Timed
   @Path("/{id}")
-  public Response findTemplate(@PathParam(PP_ID) String id) throws CedarException {
+  @Produces({MediaType.APPLICATION_JSON, "application/x-yaml"})
+  public Response findTemplate(@PathParam(PP_ID) String id,
+                               @HeaderParam("Accept") String acceptHeader,
+                               @QueryParam("compact") Optional<Boolean> compactParam,
+                               @QueryParam("download") Optional<Boolean> downloadParam) throws CedarException {
     CedarRequestContext c = buildRequestContext();
     c.must(c.user()).be(LoggedIn);
     c.must(c.user()).have(CedarPermission.TEMPLATE_READ);
     CedarTemplateId tid = CedarTemplateId.build(id);
 
     userMustHaveReadAccessToArtifact(c, tid);
-    return executeResourceGetByProxyFromArtifactServer(CedarResourceType.TEMPLATE, id, c);
+    if (acceptHeader.isEmpty() || acceptHeader.contains(MediaType.APPLICATION_JSON)) {
+      return executeResourceGetByProxyFromArtifactServer(CedarResourceType.TEMPLATE, id, c);
+    }
+    if (acceptHeader.contains("application/x-yaml")) {
+      FolderServiceSession folderSession = CedarDataServices.getFolderServiceSession(c);
+      CedarTemplateId templateId = CedarTemplateId.build(id);
+      FolderServerArtifact template = folderSession.findArtifactById(templateId);
+    }
+    return CedarResponse.badRequest()
+        .errorMessage("You passed an invalid Accept header: '" + acceptHeader + "'")
+        .errorKey(CedarErrorKey.INVALID_RESOURCE_TYPE)
+        .parameter("Accept", acceptHeader)
+        .parameter("allowed Accept headers", Arrays.toString(new String[]{MediaType.APPLICATION_JSON, "application/x-yaml"}))
+        .build();
   }
 
   @GET
